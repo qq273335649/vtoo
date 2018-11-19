5be41ef5d16091e5d6d10f44591f9afd8dfabbfc
wusong <273335649@qq.com>
2018-11-19 14:03:20 +0800

    1.1

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'mysql99aiji2';
+$cfg_dbuser = 'mysql99aiji2';
+$cfg_dbpwd = '1B28255';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
